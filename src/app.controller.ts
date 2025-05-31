44d46256801c0915ce832465f42f692f5783af29
t.berteau <thomas.berteau@egis-group.com>
2025-05-31 18:23:12 +0200

    feat: create user module and service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Post('login')
+  login(): any {
+    return {}
+  }
+
+
+  // GET /protected
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
