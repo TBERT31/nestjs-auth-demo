56a0f9c9856f6d416e31e02ec9cdfb78d70cbb7b
t.berteau <thomas.berteau@egis-group.com>
2025-05-31 18:32:30 +0200

    feat(user module ready for demo)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+    id: number;
+    name: string;
+    username: string;
+    password: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users: User[] = [
+        {
+            id: 1,
+            name: 'John Doe',
+            username: 'john.doe',
+            password: 'password123',
+        },
+        {
+            id: 2,
+            name: 'Jane Smith',
+            username: "jane.smith",
+            password: 'securepassword',
+        }
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
