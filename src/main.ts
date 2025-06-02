f9fba265ba755ed6d5e816e293bec4f8e490e9f2
t.berteau <thomas.berteau@egis-group.com>
2025-06-02 09:40:16 +0200

    indentation

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(session({
-    secret: 'session cat', // get env variables
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 3600000 },
-  }));
+  app.use(
+    session({
+      secret: 'keyboard cat', // get from env variables
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    })
+  );
 
   app.use(passport.initialize());
   app.use(passport.session())
